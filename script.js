1a0adb582ac136b109c269b1e96a3c52c7395c96
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-09 02:09:30 +0100

    completing changing edit button to hide button

--- script.js
+++ script.js
@@ -1,35 +1,100 @@
-const changePic = document.getElementsByTagName("svg");
+const changePic = document.getElementsByClassName("changeImg");
+const changeBtn = document.getElementsByClassName(
+  "btn btn-sm btn-outline-secondary edit_btn"
+);
 
-window.onload = function () {
-  for (let i = 0; i < changePic.length; i++) {
-    changePic[i].innerHTML = `<svg
-    class="bd-placeholder-img card-img-top"
-    id="changePic"
-    width="100%"
-    height="225"
-    xmlns="http://www.w3.org/2000/svg"
-    preserveAspectRatio="xMidYMid slice"
-    focusable="false"
-    role="img"
-    aria-label="Placeholder: Thumbnail"
-  >
-    <title>Placeholder</title>
-    <img src="./avatar-1.jpg" alt="..." />
-    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-      Thumbnail
-    </text>
-  </svg>`;
-  }
+const generateFirstPic = function () {
+  fetch("https://api.pexels.com/v1/search?query=[dog]", {
+    headers: {
+      Authorization: "563492ad6f9170000100000129a7fe8f077b4791b8773731d45ed786",
+    },
+  })
+    .then((response) => response.json())
+    .then((dog) => {
+      console.log(dog);
+      for (let i = 0; i < changePic.length; i++) {
+        changePic[i].innerHTML = `
+      <div class="card mb-4 shadow-sm img_change">
+                <img src="${dog.url}" alt="..." />
+                <div class="card-body">
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class="btn-group">
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+      `;
+      }
+    })
+    .catch((error) => console.error(error));
 };
 
-fetch("https://api.pexels.com/v1/search?query=people", {
-  headers: {
-    Authorization: "563492ad6f9170000100000129a7fe8f077b4791b8773731d45ed786",
-  },
-})
-  .then((resp) => {
-    return resp.json();
+const generateSecondaryPic = function () {
+  fetch("https://api.pexels.com/v1/search?query=[cat]", {
+    headers: {
+      Authorization: "563492ad6f9170000100000129a7fe8f077b4791b8773731d45ed786",
+    },
   })
-  .then((data) => {
-    console.log(data);
-  });
+    .then((response) => response.json())
+    .then((cat) => {
+      console.log(cat);
+      for (let i = 0; i < changePic.length; i++) {
+        changePic[i].innerHTML = `
+      <div class="card mb-4 shadow-sm img_change">
+                <img src="${cat.url}" alt="..." />
+                <div class="card-body">
+                  <p class="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class="btn-group">
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+      `;
+      }
+    })
+    .catch((error) => console.error(error));
+};
+
+const changeButton = function (get_id) {
+  changeBtn[get_id - 1].innerHTML = `Hide`;
+};
